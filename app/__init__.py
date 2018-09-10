121e8fe9d622f5ce94a737222085671948f310ae
joost <joost823@gmail.com>
2018-09-10 12:36:31 +0200

    added sqlalchemy example

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
-from flask import Flask, redirect, session
-from app.mod_tables.models import TableBuilder
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 flask_app = Flask(__name__)
+db = SQLAlchemy(flask_app)
+
+from app.mod_tables.models import TableBuilder, SomeTable, add_some_random_db_entries
+
+db.create_all()
+
+add_some_random_db_entries()
 
 table_builder = TableBuilder()
 
--- app/mod_tables/models.py
+++ app/mod_tables/models.py
@@ -1,5 +1,6 @@
 from app.mod_tables.serverside.serverside_table import ServerSideTable
 from app.mod_tables.serverside import table_schemas
+from app import db
 
 DATA_SAMPLE = [
     {'A': 'Hello!', 'B': 'How is it going?', 'C': 3, 'D': 4},
@@ -10,6 +11,44 @@ DATA_SAMPLE = [
     {'A': 'Thanks!', 'B': 'Goodbye.', 'C': 12, 'D': 13}
 ]
 
+class SomeTable(db.Model):
+    __tablename__ = 'some_table'
+    cola = db.Column('A', db.String(2))
+    colb = db.Column('B', db.String(2))
+    colc = db.Column('C', db.Integer, primary_key=True)
+    cold = db.Column('D', db.Integer)
+
+    def __init__(self, cola, colb, colc, cold):
+        self.cola = cola
+        self.colb = colb
+        self.colc = colc
+        self.cold = cold
+
+    @property
+    def serialize(self):
+        return {
+             'A': self.cola,
+             'B': self.colb,
+             'C': self.colc,
+             'D': self.cold
+        }
+
+
+def add_some_random_db_entries():
+    letters = 'arstarstarstarstaars'
+    for i in range(10):
+        item = SomeTable(letters[i], letters[i + 1: i + 3], i, i + 1)
+        db.session.add(item)
+    db.session.commit()
+
+
+def make_data_sample_from_db():
+    newlist = []
+    for row in SomeTable.query.all():
+        newlist.append(row.serialize)
+    return newlist
+
+
 class TableBuilder(object):
 
     def collect_data_clientside(self):
@@ -17,4 +56,5 @@ class TableBuilder(object):
 
     def collect_data_serverside(self, request):
         columns = table_schemas.SERVERSIDE_TABLE_COLUMNS
-        return ServerSideTable(request, DATA_SAMPLE, columns).output_result()
+        data = make_data_sample_from_db()
+        return ServerSideTable(request, data, columns).output_result()
